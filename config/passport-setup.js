20c04a3845d123fac1161f796f4459ca843cb6c0
shahid <133004142+shahidit236@users.noreply.github.com>
2023-09-29 22:14:27 +0530

    first commit

---
+++ config/passport-setup.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+let mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+require('dotenv').config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      // options for strategy
+      callbackURL: 'http://localhost:3000/auth/google/callback/',
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const email = profile.emails[0].value;
+
+      // check if user already exists
+      const currentUser = await User.findOne({ googleId: profile.id });
+      if (currentUser) {
+        // already have the user -> return (login)
+        return done(null, currentUser);
+      } else {
+        // register user and return
+        const newUser = await new User({ email: email, googleId: profile.id }).save();
+        return done(null, newUser);
+      }
+    }
+  )
+);
